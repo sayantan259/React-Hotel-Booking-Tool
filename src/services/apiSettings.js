ecfe2fcc42ec213fe7fcb832a85b63e664912e78
Sayantan Bairagi <sayantan259@gmail.com>
2025-04-05 14:11:09 +0530

    Settings can be updated in real time

--- src/services/apiSettings.js
+++ src/services/apiSettings.js
@@ -1,13 +1,13 @@
 import supabase from "./supabase";
 
 export async function getSettings() {
-  const { data, error } = await supabase.from("settings").select("*").single();
+  const { data, error } = await supabase.from("settings").select("*");
 
   if (error) {
     console.error(error);
     throw new Error("Settings could not be loaded");
   }
-  return data;
+  return data[0];
 }
 
 // We expect a newSetting object that looks like {setting: newValue}
